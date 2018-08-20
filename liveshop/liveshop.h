28e4bb69e1cfe590d8f5a587d48dbce12872e949
Renan Kosicki <renankosicki@gmail.com>
2018-08-20 19:07:51 -0300

    Initial Commit

---
+++ liveshop/liveshop.h
@@ -0,0 +1,19 @@
+//
+//  liveshop.h
+//  liveshop
+//
+//  Created by Renan Kosicki on 20/08/18.
+//  Copyright © 2018 LiveShopApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for liveshop.
+FOUNDATION_EXPORT double liveshopVersionNumber;
+
+//! Project version string for liveshop.
+FOUNDATION_EXPORT const unsigned char liveshopVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <liveshop/PublicHeader.h>
+
+
